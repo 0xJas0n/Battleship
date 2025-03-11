5a85c4c15dc17979e1e5d8919816e6543d555f37
Jason Lang <raptorify@gmail.com>
2025-03-11 19:11:16 +0100

    Remove unused files

--- src/main/java/jl/battleship/domain/Game.java
+++
@@ -1,7 +0,0 @@
-package jl.battleship.domain;
-
-public class Game {
-    private Long gameId;
-    private Player player1;
-    private Player player2;
-}
--- src/main/java/jl/battleship/domain/Ship.java
+++
@@ -1,7 +0,0 @@
-package jl.battleship.domain;
-
-public class Ship {
-    private int startIndex;
-    private int size;
-    private boolean isHorizontal;
-}
